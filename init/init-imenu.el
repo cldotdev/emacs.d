22437eee6d7786136a99be025092a7aa73ab4a8d
Chienlung Huang <hi@chienlunghuang.com>
2026-02-02 11:09:20 +0800

    revert(imenu): disable custom RET behavior in imenu-list
    Comment out the jump-and-quit function to restore default RET behavior,
    keeping the imenu-list window open after navigating to an entry.

--- init/init-imenu.el
+++ init/init-imenu.el
@@ -4,16 +4,16 @@
 (global-set-key (kbd "C-c i") #'imenu-list-smart-toggle)
 (setq imenu-list-focus-after-activation t)
 
-(with-eval-after-load 'imenu-list
-  (defun my/imenu-list-ret-dwim-and-quit ()
-    "Jump to entry at point and close the imenu-list window.
-For subalist entries, toggle folding instead."
-    (interactive)
-    (let ((entry (imenu-list--find-entry)))
-      (if (imenu--subalist-p entry)
-          (hs-toggle-hiding)
-        (imenu-list--goto-entry entry)
-        (imenu-list-minor-mode -1))))
-  (define-key imenu-list-major-mode-map (kbd "RET") #'my/imenu-list-ret-dwim-and-quit))
+;; (with-eval-after-load 'imenu-list
+;;   (defun my/imenu-list-ret-dwim-and-quit ()
+;;     "Jump to entry at point and close the imenu-list window.
+;; For subalist entries, toggle folding instead."
+;;     (interactive)
+;;     (let ((entry (imenu-list--find-entry)))
+;;       (if (imenu--subalist-p entry)
+;;           (hs-toggle-hiding)
+;;         (imenu-list--goto-entry entry)
+;;         (imenu-list-minor-mode -1))))
+;;   (define-key imenu-list-major-mode-map (kbd "RET") #'my/imenu-list-ret-dwim-and-quit))
 
 (provide 'init-imenu)
